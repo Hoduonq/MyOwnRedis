1c86a20f95daac2fd231d240cabb76abd31af061
Hoduonq <hoduonq@gmail.com>
2025-04-26 15:50:24 +0700

    stage 2

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
     pong = "+PONG\r\n"
     con, add = server_socket.accept()
     
