455ea02ac5c5ef6f4fbad78e688d2b5df492a3d8
Hoduonq <hoduonq@gmail.com>
2025-04-26 14:29:26 +0700

    stage2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-
+    con, add = server_socket.accept()
+    
+    with conn: 
+        conn.recv(1024)
+        conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
