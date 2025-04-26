2828ab0c476a9255b2c402bf3d904d67c47c8f93
Hoduonq <hoduonq@gmail.com>
2025-04-26 15:50:45 +0700

    stage 2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     pong = "+PONG\r\n"
-    con, add = server_socket.accept()
+    conn, add = server_socket.accept()
     
     with conn: 
         conn.recv(1024)
